3294da5f7487981f298447127cf25ab7185e5a72
Aldiyansyah-dev <aldiyansyah.dev@gmail.com>
2021-06-17 02:53:44 +0700

    menambahkan assets bootstrap kedalam mvc

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
-    <h1>About Me</h1>
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/aldi.jpg" alt="aldi" width="200" class="rounded-circle shadow">
     <p>Hallo, Nama Saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun. pekerjaan saya <?= $data['pekerjaan']; ?></p>
+</div>
